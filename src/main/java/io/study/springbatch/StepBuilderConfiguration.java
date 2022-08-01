13a522d790fb7e49417f52cc179af0b8b106a60d
moon-mac <bbangssi91@gmail.com>
2022-08-01 22:41:02 +0900

    part2-2) Stepbuilder chunk example

--- src/main/java/io/study/springbatch/StepBuilderConfiguration.java
+++ src/main/java/io/study/springbatch/StepBuilderConfiguration.java
@@ -5,14 +5,13 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.builder.FlowBuilder;
-import org.springframework.batch.core.job.flow.Flow;
-import org.springframework.batch.item.*;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
+import java.util.Arrays;
 
 @RequiredArgsConstructor
 @Configuration
@@ -33,9 +32,7 @@ public class StepBuilderConfiguration {
     public Step step1() {
         return stepBuilderFactory.get("step1")
             .tasklet((stepContribution, chunkContext) -> {
-
                 System.out.println("step1");
-
                 return RepeatStatus.FINISHED;
             })
             .build();
@@ -45,64 +42,16 @@ public class StepBuilderConfiguration {
     public Step step2() {
         return stepBuilderFactory.get("step2")
                 .<String, String>chunk(3)
-                .reader(new ItemReader<String>() {
-                    @Override
-                    public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
-                        return null;
-                    }
-                })
-                .processor(new ItemProcessor<String, String>() {
-                    @Override
-                    public String process(String s) throws Exception {
-                        return null;
-                    }
-                })
-                .writer(new ItemWriter<String>() {
-                    @Override
-                    public void write(List<? extends String> list) throws Exception {
-
-                    }
-                })
+                .reader(new ListItemReader<>(Arrays.asList(
+                        "item1",
+                        "item2",
+                        "item3",
+                        "item4",
+                        "item5"
+                )))
+                .processor((ItemProcessor<String, String>) String::toUpperCase)
+                .writer(list -> list.forEach(System.out::println))
                 .build();
-
-    }
-
-    @Bean
-    public Step step3(){
-        return stepBuilderFactory.get("step3")
-                .partitioner(step1())
-                .gridSize(2)
-                .build();
-    }
-
-    @Bean
-    public Step step4(){
-        return stepBuilderFactory.get("step4")
-                .job(job())
-                .build();
-    }
-
-    @Bean
-    public Step step5(){
-        return stepBuilderFactory.get("step5")
-                .flow(flow())
-                .build();
-    }
-
-    @Bean
-    public Job job() {
-        return jobBuilderFactory.get("job")
-                .start(step1())
-                .next(step2())
-                .next(step3())
-                .build();
-    }
-
-    @Bean
-    public Flow flow(){
-        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
-        flowBuilder.start(step2()).end();
-        return flowBuilder.build();
     }
 
 }
